5533761b6657967031df90a5f0c9cd2485f990cc
Yossi Workd WarZone Control <119104194+YossiKat@users.noreply.github.com>
2026-04-26 13:18:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/warwatch-pro/", 
+base: "/warwatch-pro/",
   plugins: [react()],
   resolve: {
     alias: {
